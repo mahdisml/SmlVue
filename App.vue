a850208676011f55b4b484863a2d2058b8e55ea5
Mahdi Safarmohammadloo <mahdisml98@gmail.com>
2021-12-01 22:54:07 +0330

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+import { ref } from "vue";
+
+const color = ref<string>("blue")
+
+function setRed(){
+  color.value = "red"
+}
+
+</script>
+
+<!-- --------------------------------------------------- -->
+
+<template>
+
+<div class="div1">
+  <button v-on:click="setRed">Change Color !</button>
+</div>
+
+</template>
+
+<!-- --------------------------------------------------- -->
+
+<style scoped>
+
+.div1{
+  background-color: v-bind(color);
+}
+
+</style>
