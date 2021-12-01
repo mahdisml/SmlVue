cff0e9d61834b28989ca848ac4b9af9b88052878
Mahdi Safarmohammadloo <mahdisml98@gmail.com>
2021-12-01 22:51:11 +0330

    Update store.ts

--- store.ts
+++ store.ts
@@ -1,3 +1,8 @@
 import { ref } from "vue";
 
 export const store_count = ref<number>(0)
+
+// no need for it , you can set it in every where !
+export function increase (num:number) {
+    store_count.value = store_count.value + num
+}
