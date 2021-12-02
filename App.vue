f5f9671ebbff1052eac5d57a78a34b45abd5e719
Mahdi Safarmohammadloo <mahdisml98@gmail.com>
2021-12-03 02:04:36 +0330

    Update App.vue

--- App.vue
+++ App.vue
@@ -6,18 +6,29 @@ import Home from './Home.vue';
 <!-- --------------------------------------------------- -->
 
 <template>
+
 <div>
    <Home/> 
 </div>
+
 </template>
 
 <!-- --------------------------------------------------- -->
 
 <style>
 
-html,body { 
-    padding: 0;
-    margin: 0;
-    border: 0 none;
+html,body {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 }
+a {
+  color: inherit;
+  text-decoration: none;
+}
+* {
+  box-sizing: border-box;
+}
+
 </style>
