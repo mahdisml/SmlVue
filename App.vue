8619af493382d0c81235b23c194dc698838a30c2
Mahdi Safarmohammadloo <mahdisml98@gmail.com>
2021-12-03 02:01:28 +0330

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Home from './Home.vue';
+
+</script>
+
+<!-- --------------------------------------------------- -->
+
+<template>
+<div>
+   <Home/> 
+</div>
+</template>
+
+<!-- --------------------------------------------------- -->
+
+<style>
+
+html,body { 
+    padding: 0;
+    margin: 0;
+    border: 0 none;
+}
+</style>
